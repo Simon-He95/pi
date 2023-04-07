3a5343c719d23bd342a98f1a3d64f5755b0f6b68
Simon He <13917107469@163.com>
2023-04-07 13:30:45 +0800

    chore: update help

--- src/help.ts
+++ src/help.ts
@@ -7,26 +7,16 @@ export function help(argv: any[]) {
     jsShell(`gum style \
     --foreground 212 --border-foreground 212 --border double \
     --align center --width 50 --margin "1 2" --padding "2 4" \
-    'pi version:${version}' 'Please give me a 🌟 for my efforts'`)
+    "pi version: ${version}" "Please give me a 🌟 for my efforts" "Thank you 🤟"`)
     process.exit(0)
   }
   else if (arg === '-h' || arg === '--help') {
     jsShell(
-      'gum style \
+      `gum style \
     --foreground 212 --border-foreground 212 --border double \
     --align left --width 50 --margin "1 2" --padding "1 1" \
-    \'PI Commands:\'\
-    \'~ pi: install package\'\
-    \'~ pix: npx package\'\
-    \'~ pui: uninstall package\'\
-    \'~ prun: run package script\'\
-    \'~ pinit: package init\'\
-    \'~ pbuild: go build | cargo build\' \
-    \'~ pfind: find monorepo of yarn or pnpm\'\
-    \'~ pa: agent alias\'\
-    \'~ pu: package upgrade\'\
-    \'~ pci: package clean install\'\
-    \'~ pil: package latest install\'',
+    'PI Commands:' '~ pi: install package' '~ pix: npx package' '~ pui: uninstall package' '~ prun: run package script' '~ pinit: package init' '~ pbuild: go build | cargo build' '~ pfind: find monorepo of yarn or pnpm' '~ pa: agent alias' '~ pu: package upgrade' '~ pci: package clean install' '~ pil: package latest install'
+  `,
     )
     process.exit(0)
   }
