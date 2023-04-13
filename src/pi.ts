2d0133815d4465a06bc985b0b7d45ceee5b46a40
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-04-13 15:36:46 +0800

    fix(yarn):  networkConcurrency

--- src/pi.ts
+++ src/pi.ts
@@ -41,11 +41,7 @@ export async function pi(params: string, pkg: string, executor = 'ni') {
   }
 
   const runSockets
-    = executor.split(' ')[0] === 'npm'
-      ? ` --max-sockets=${maxSockets}`
-      : pkgTool === 'yarn'
-        ? ` --mutex network --network-concurrency ${maxSockets}`
-        : ''
+    = executor.split(' ')[0] === 'npm' ? ` --max-sockets=${maxSockets}` : ''
   const { status, result } = await useNodeWorker({
     params: `${executor}${newParams ? ` ${newParams}` : runSockets}`,
     stdio,
