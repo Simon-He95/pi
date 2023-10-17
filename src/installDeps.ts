bef3fbcb9573ec48d43b4514e9db454d8996d51e
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-10-17 11:10:47 +0800

    chore: 增加安装失败日志

--- src/installDeps.ts
+++ src/installDeps.ts
@@ -5,12 +5,32 @@ import color from 'picocolors'
 export async function installDeps() {
   if (!isInstallPkg('gum')) {
     console.log(color.cyan('正在为您安装必要的依赖gum...'))
-    await jsShell('brew install gum', 'pipe')
-    console.log(color.cyan('gum 安装成功!'))
+    const { status } = await jsShell('brew install gum', 'pipe')
+    if (status === 0) {
+      console.log(color.cyan('gum 安装成功!'))
+    }
+    else {
+      console.log(
+        color.red(
+          'gum 安装失败，请尝试从官网解决安装问题! https://github.com/charmbracelet/gum',
+        ),
+      )
+      process.exit(1)
+    }
   }
   if (!isInstallPkg('ni')) {
     console.log(color.cyan('正在为您安装必要的依赖ni...'))
-    await jsShell('npm i -g @antfu/ni', 'pipe')
-    console.log(color.cyan('ni 安装成功!'))
+    const { status } = await jsShell('npm i -g @antfu/ni', 'pipe')
+    if (status === 0) {
+      console.log(color.cyan('ni 安装成功!'))
+    }
+    else {
+      console.log(
+        color.red(
+          'ni 安装失败，请尝试从官网解决安装问题! https://github.com/antfu/ni',
+        ),
+      )
+      process.exit(1)
+    }
   }
 }
