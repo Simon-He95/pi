28bb7194551dc12f5a63d3931d164f2f75e0aace
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-14 09:29:51 +0800

    chore: update

--- src/pil.ts
+++ src/pil.ts
@@ -1,7 +1,41 @@
+import { getPkg, jsShell } from 'lazy-js-utils'
+import pc from 'picocolors'
 import { pi } from './pi'
 import { getParams } from './utils'
 // install @latest
 export async function pil(params: string) {
+  if (!params) {
+    // 提供当前所有依赖选择
+    const { dependencies, devDependencies } = await getPkg()
+
+    const deps = [
+      ...Object.keys(dependencies).map(key => `${key}: ${dependencies[key]}`),
+      ...Object.keys(devDependencies).map(
+        key => `${key}: ${devDependencies[key]}`,
+      ),
+    ]
+    const { result: choose, status } = jsShell(
+      `echo ${deps.join(
+        ',',
+      )} | sed "s/,/\\n/g" | gum filter --placeholder=" 🤔${
+        process.env.PI_Lang === 'zh'
+          ? '请选择一个依赖获取最新版本'
+          : 'Please select a dependency to get the latest version.'
+      }"`,
+      'pipe',
+    )
+    if (status === 130) {
+      console.log(pc.dim('已取消'))
+      process.exit(0)
+    }
+    else if (status !== 0) {
+      throw new Error(choose)
+    }
+    const name = choose.split(': ')[0]
+    if (name in devDependencies)
+      params = `${name} -D`
+    else params = name
+  }
   let latestPkgname = addLatest(params)
   let suffix = ''
   const reg = /\s(-[dDwW]+)/g
