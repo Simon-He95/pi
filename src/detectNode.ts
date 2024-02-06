d0b99931fcdb4fc72851cf983321052a4601a51d
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-02-06 11:53:22 +0800

    chore: 优化错误消息

--- src/detectNode.ts
+++ src/detectNode.ts
@@ -1,11 +1,22 @@
 import process from 'process'
 import { getPkg, jsShell } from 'lazy-js-utils'
+import colors from 'picocolors'
+
 const semver = require('semver')
 
 export async function detectNode() {
-  const pkg = await getPkg()
+  let pkg
+  try {
+    pkg = await getPkg()
+  }
+  catch (e: any) {
+    const cwd = process.cwd()
+    console.log(colors.red(`当前目录: ${cwd} 没有package.json文件`))
+    process.exit(1)
+  }
   if (pkg.engines?.node) {
     const isSafe = semver.satisfies(process.version, pkg.engines.node)
+
     if (!isSafe) {
       const { result, status } = await jsShell(
         `echo "yes\nno" | gum filter --placeholder=" 当前node版本不满足 ${pkg.engines.node}，是否切换node版本"`,
@@ -14,13 +25,13 @@ export async function detectNode() {
       if (status === 0 && result === 'yes') {
         await jsShell(
           `
-         current=$(echo $(fnm current))
-         registery=$(echo "$(fnm ls)" | sed 's/system//g' | sed 's/default//g' | sed 's/\* //g' | sed "s/$current/\* $current/g" | gum filter --placeholder=" 请选择一个node版本")
-         registery=$(echo $\{registery// /} | sed 's/\*//g')
-         if [ $registery ]; then
-          fnm use $\{registery% -*}
-         fi
-        `,
+           current=$(echo $(fnm current))
+           registery=$(echo "$(fnm ls)" | sed 's/system//g' | sed 's/default//g' | sed 's/\* //g' | sed "s/$current/\* $current/g" | gum filter --placeholder=" 请选择一个node版本")
+           registery=$(echo $\{registery// /} | sed 's/\*//g')
+           if [ $registery ]; then
+            fnm use $\{registery% -*}
+           fi
+          `,
           'pipe',
         )
       }
